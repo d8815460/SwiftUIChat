5f706ff51a954435bd0769c1a62a361db9629d57
Jobs <jobs@seektopser.com>
2022-12-15 22:40:44 +0800

    Initial Commit

---
+++ SwiftUIChat/SwiftUIChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIChatApp.swift
+//  SwiftUIChat
+//
+//  Created by Ayi on 2022/12/15.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
