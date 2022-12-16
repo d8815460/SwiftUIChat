5391ad1e3d539565c9311127d2ecead1ee8886f8
Jobs <jobs@seektopser.com>
2022-12-16 13:20:26 +0800

    [Fix] 簡化，移除 AppDelegate

--- SwiftUIChat/SwiftUIChatApp.swift
+++ SwiftUIChat/SwiftUIChatApp.swift
@@ -8,19 +8,24 @@
 import SwiftUI
 import FirebaseCore
 
-class AppDelegate: NSObject, UIApplicationDelegate {
-  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-      FirebaseApp.configure()
-    return true
-  }
-}
+//class AppDelegate: NSObject, UIApplicationDelegate {
+//  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+//      FirebaseApp.configure()
+//    return true
+//  }
+//}
 
 @main
 struct SwiftUIChatApp: App {
-    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+//    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
+    init() {
+        FirebaseApp.configure()
+    }
+
     var body: some Scene {
         WindowGroup {
-            MainTabView()
+            LoginView()
         }
     }
 }
